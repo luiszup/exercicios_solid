a00e3a6afac6052f63194ee6e63574adca9a2c6c
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-01-08 15:18:58 -0300

    feat: adiciona classe ContaCorrente que se extende de Conta e coloca a taxa fixa para conta corrente em seu método

---
+++ src/bonus/ContaCorrente.java
@@ -0,0 +1,11 @@
+package bonus;
+
+public class ContaCorrente extends Conta{
+    public ContaCorrente(double saldoInicial) {
+        super(saldoInicial);
+    }
+
+    public double calcularTaxa() {
+        return 5.0;
+    }
+}
